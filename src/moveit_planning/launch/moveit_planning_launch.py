4f6301095226832b88f58ff8504a56ebb4d58c7d
Alex Cetanas <116481552+Alex-cet@users.noreply.github.com>
2025-01-14 21:21:29 +0200

    Added keyboard input publishing to Twist topic (#2)

--- src/moveit_planning/launch/moveit_planning_launch.py
+++ src/moveit_planning/launch/moveit_planning_launch.py
@@ -12,4 +12,16 @@ def generate_launch_description():
                 {'use_sim_time': True}
             ]
         ),
+        Node(
+            package='keyboard_control',
+            executable='ur5_keyboard_control.py',
+            name='ur5_keyboard_control',
+            output='screen'
+        ),
+        Node(
+            package='keyboard_control',
+            executable='read_twist_topic.py',
+            name='read_twist_topic',
+            output='screen'
+        ),
     ])
